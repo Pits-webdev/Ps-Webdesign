56109c434b4bed4cb54ed8aafcd48d70ed2bdfb6
Pits-webdev <pits.webdev@gmail.com>
2025-12-03 09:07:47 +0100

    v  0.0.1

---
+++ src/data/data.ts
@@ -0,0 +1,29 @@
+
+ export const filterButtons = [
+  {
+    name: "All",
+    count: 120,
+    isActive: true
+  },
+  {
+    name: "Summer Collection",
+    count: 45,
+    isActive: false
+  },
+  {
+    name: "Best Sell",
+    count: 30,
+    isActive: false
+  },
+  {
+    name: "New Ariv",
+    count: 20,
+    isActive: false
+  },
+ 
+  {
+    name: "Limited",
+    count: 8,
+    isActive: false
+  }
+];
